e1e0822013b04fd5451915a5afef40250235780a
Teemu Taivainen <teemu.taivainen@aalto.fi>
2023-12-22 14:48:45 +0200

    Test contour plot

--- pde-visualizer-frontend/src/app/ContourPlot.tsx
+++ pde-visualizer-frontend/src/app/ContourPlot.tsx
@@ -3,16 +3,29 @@
 import dynamic from 'next/dynamic'
 const Plot = dynamic(() => import('react-plotly.js'), { ssr: false })
 
+// Function to calcuate length of a vector, given x and y components
+const vectorLength = (x: number, y: number) => {
+  return Math.sqrt(x * x + y * y)
+}
+
+// Function to calculate value of Fundamental solution of Laplace's equation
+const phi = (x: number, y: number) => {
+  return - 1/(2*Math.PI) * Math.log(vectorLength(x, y))
+}
+
+function linspace(start: number, end: number, num: number) {
+  const step = (end - start) / (num - 1);
+  return Array.from({length: num}, (_, i) => start + (i * step));
+}
+
+function zip(array1: number[], array2: number[]): [number, number][] {
+  return array1.map((value, index) => [value, array2[index]]);
+}
+
 const ContourPlot = () => {
-  const x = [1, 2, 3, 4, 5]
-  const y = [1, 2, 3, 4, 5]
-  const z = [
-    [1, 2, 3, 4, 5],
-    [2, 3, 4, 5, 6],
-    [3, 4, 5, 6, 7],
-    [4, 5, 6, 7, 8],
-    [5, 6, 7, 8, 9]
-  ]
+  const x = linspace(-5, 5, 100)
+  const y = linspace(-5, 5, 100)
+  const z = zip(x, y).map(([x, y]) => phi(x, y))
 
   return (
     <Plot
